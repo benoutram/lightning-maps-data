656ee76d6a7c6a4acd0a553533704cde2bc1099f
Ben Outram <ben@outram.com>
2014-01-26 16:48:18 +0000

    Removing leading zeros from the postcode district.

--- postcode_districts/add_to_penalty_postcodes.js
+++ postcode_districts/add_to_penalty_postcodes.js
@@ -1,5 +1,9 @@
 db.penaltyPostcode.find().forEach(function(penaltyPostcodeDoc) {
-	var postcodeDistrictDoc = db.postcodeDistricts.findOne({"PCDistrict" : penaltyPostcodeDoc.District}, {"Latitude" : 1, "Longitude" : 1});
+	var district = penaltyPostcodeDoc.District;
+	//remove any leading zeros from the district
+	district = district.replace(/^([a-z]+)(0)(.+)/i,"$1" + "$3");
+
+	var postcodeDistrictDoc = db.postcodeDistricts.findOne({"PCDistrict" : district}, {"Latitude" : 1, "Longitude" : 1});
 	if (postcodeDistrictDoc) {
 		db.penaltyPostcode.update(
 			{ "_id" : penaltyPostcodeDoc._id},
